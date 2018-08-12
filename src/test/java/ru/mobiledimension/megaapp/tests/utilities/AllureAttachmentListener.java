718519c2f1aa881fa54ed57200f974dc334f30f4
Anton <agalkin@mobiledimension.ru>
2018-08-12 21:47:13 +0300

    Fix incorrect class initialization

--- src/test/java/ru/mobiledimension/megaapp/tests/utilities/AllureAttachmentListener.java
+++ src/test/java/ru/mobiledimension/megaapp/tests/utilities/AllureAttachmentListener.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.sql.Timestamp;
 
-public abstract class AllureAttachmentListener extends BaseTest implements ISuiteListener, IInvokedMethodListener {
+public class AllureAttachmentListener extends BaseTest implements ISuiteListener, IInvokedMethodListener {
 
     public void onStart(ISuite suite) {
         System.out.println("Before executing suite: " + suite.getName());
